cad6fa4825a37ca7c44f9129d7c26d505e0060cc
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2018-08-17 15:43:44 +0200

    inital release

---
+++ README.md
@@ -0,0 +1,23 @@
+BaaSify
+========
+> a Backend-as-a-Service based on `ParsePlatform` with the abbility of multi-tenant "multi-apps" in the same server.
+
+Why
+====
+> I wanted to manage multiple parse apps on the same instance.
+
+How it works
+============
+> `BaaSify` uses `redis` to load the applications, you can add application using redis:
+
+```bash
+$ redis-cli hset apps app1 '{"appId": "app1", "appSecret": "app1-secret-string"}'
+```
+
+Notes
+======
+> You can use `pm2` workers to utilize the full power of your CPUs.
+
+Author
+======
+Mohamed Al Ashaal :)
---
+++ main.js
@@ -0,0 +1,80 @@
+/**
+ * BaaSify a parse-server based backend-as-a-service with multi-apps feature.
+ * 
+ * @author Mohamed Al Ashaal <m7medalash3al@gmail.com>
+ * @version 1.0.0
+ */
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const conf = require('dotenv').config().parsed;
+const redis = require('redis').createClient(conf.REDIS_DSN);
+const fs = require('fs');
+const mux = express();
+
+/**
+ * Available deployed apps
+ */
+var deployments = {};
+
+/**
+ * Deploy the specified app
+ * 
+ * @param {Object} config 
+ * @param {Closure} fn 
+ */
+function deployApp(config, fn) {
+    fs.mkdir('./cloud-functions/'+config.appId, function(){
+        fs.writeFile('./cloud-functions/'+config.appId+'/main.js', '', function(){
+            var api = new ParseServer({
+                databaseURI: conf.MONGO_DSN+"/baas_"+config.appId,
+                cloud: './cloud-functions/'+config.appId+'/main.js',
+                appId: config.appId,
+                masterKey: config.appSecret
+            })
+            deployments[config.appId] = {
+                api: api,
+                config: config
+            }
+            fn && fn(api);
+        })
+    })
+}
+
+/**
+ * Start serving the registered apps
+ */
+mux.use('/parse', function(req, res, next){
+    var appId = req.get("X-Parse-Application-Id")
+
+    var success = function(api){
+        api(req, res, next)
+    }
+
+    var reject = function(){
+        res.status(404).json({
+            error: "invalid application specified"
+        })
+    }
+
+    if ( deployments[appId] ) {
+        success(deployments[appId].api)
+    } else {
+        redis.hget("apps", appId, function(_, val){
+            if ( ! val ) {
+                reject()
+                return
+            }
+            var info = JSON.parse(val)
+            deployApp(info, function(api){
+                success(api)
+            })
+        })
+    }
+})
+
+/**
+ * Start listening on the configured port
+ */
+mux.listen(parseInt(conf.HTTP_PORT), function() {
+  console.log('⇨ http server started on port '+conf.HTTP_PORT);
+});
